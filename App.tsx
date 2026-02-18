ae9b393906b13e463bc7ab50f89f7808724f9e7a
Hardik Yadav <hardikyadav@Hardik-Yadav.local>
2026-02-18 19:54:45 +0530

    initial UI

--- App.tsx
+++ App.tsx
@@ -1,38 +1,43 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
   return (
     <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
+      <SafeAreaView style={styles.container}>
+        {/* Header */}
+        <View style={styles.heading}>
+          <Text style={styles.title}>Stormee</Text>
+        </View>
+        {/* CHAT VIEW  */}
+
+        <View style={styles.chatView} >
+          <Text>Conversation will come here ...</Text>
+        </View>
+        {/* FOOTER */}
+        <View style={styles.footer}>
+          <View style={styles.audiobox}>
+          <View style={styles.speakerComponent}>
+          <Text style={styles.speaker} >Speaker</Text>
+
+          </View>
+
+                    <View style={styles.audioComponents}>
+
+          </View>
+          </View>
+
+          <TextInput
+          placeholder='your message to stormee'
+          style={styles.TextInput}
+          >
+            
+          </TextInput>
+        </View>
+
+
+      </SafeAreaView>
     </SafeAreaProvider>
-  );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
   );
 }
 
@@ -40,6 +45,70 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+
+  title: {
+    fontWeight: 'bold',
+    fontSize: 24,
+  },
+
+  heading: {
+    padding: 12,
+    borderBottomColor: 'gray',
+    borderBottomWidth: 1,
+  },
+
+  chatView: {
+    flex: 1,
+    padding: 12,
+    backgroundColor: "#fdf2f8"
+  },
+
+  footer: {
+    borderTopColor: 'gray',
+    borderTopWidth: 1,
+    padding: 12,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
+
+  TextInput: {
+    width: '100%',
+    margin: 2,
+    height: 40,
+    borderWidth: 1,
+    borderBlockColor: 'gray',
+    borderRadius: 5,
+  },
+  audiobox: {
+    flexDirection: "row",
+    justifyContent: "space-between"
+
+
+
+  },
+  audioComponents: {
+    height: 30,
+    width: "60%",
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderBlockColor: "gray"
+  },
+  speakerComponent:{
+    flex: 1,
+    backgroundColor: "gray",
+    justifyContent: "center"
+  },
+  speaker:{
+    
+  }
+
 });
 
 export default App;
+
+
+
+
+
+
